1b0c2df30446e19925f957eff1292a0ae4e2df09
Rahul11111134 <rajputr51903@gmail.com>
2026-03-02 20:24:34 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-module.exports = nextConfig
+
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+      crypto: false,
+    };
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
